60321aadfd75f3b42891e1d9b342902eeea0fe56
nortages <segatron99@gmail.com>
2019-08-08 21:55:01 +0300

    change db

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogdb',
-        'USER': 'sega',
-        'PASSWORD': 'impulse123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'blogdb',
+        # 'USER': 'sega',
+        # 'PASSWORD': 'impulse123',
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
